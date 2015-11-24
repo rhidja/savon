861d1b67f84e309b67d470fd4505b8e924956534
rhidja <rhidja@gmail.com>
2015-11-24 23:25:46 +0100

    Affichage des produits à la page d'acceuil

--- src/Ram/SavonBundle/Controller/ProduitController.php
+++ src/Ram/SavonBundle/Controller/ProduitController.php
@@ -13,7 +13,28 @@ class ProduitController extends Controller
 			throw new NotFoundHttpException('Page "'.$page.'" inexistante.');
 		}
 
-		return $this->render('RamSavonBundle:Produit:index.html.twig', array( 'listAdverts' => array() ));
+		$listProduits = array(
+			array(
+				'title'   => 'Recherche développpeur Symfony2',
+				'id'      => 1,
+				'author'  => 'Alexandre',
+				'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
+				'date'    => new \Datetime()),
+			array(
+				'title'   => 'Mission de webmaster',
+				'id'      => 2,
+				'author'  => 'Hugo',
+				'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
+				'date'    => new \Datetime()),
+			array(
+				'title'   => 'Offre de stage webdesigner',
+				'id'      => 3,
+				'author'  => 'Mathieu',
+				'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
+				'date'    => new \Datetime())
+			);
+    
+		return $this->render('RamSavonBundle:Produit:index.html.twig', array( 'listProduits' => $listProduits ));
 	}
 
 	public function viewAction($id)
