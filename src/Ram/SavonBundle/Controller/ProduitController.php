b04947c9b9eadac31800648b24d0f1644f77b6e6
rhidja <rhidja@gmail.com>
2015-11-24 22:41:37 +0100

    Ajout des différentes actions correspondantes au routes définies

--- src/Ram/SavonBundle/Controller/ProduitController.php
+++ src/Ram/SavonBundle/Controller/ProduitController.php
@@ -6,8 +6,45 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ProduitController extends Controller
 {
-    public function indexAction()
-    {
-        return $this->render('RamSavonBundle:Produit:index.html.twig');
-    }
+	public function indexAction($page)
+	{
+		if ($page < 1) 
+		{
+			throw new NotFoundHttpException('Page "'.$page.'" inexistante.');
+		}
+		
+		return $this->render('RamSavonBundle:Produit:index.html.twig');
+	}
+
+	public function viewAction($id)
+	{
+		return $this->render('RamSavonBundle:Produit:view.html.twig', array('id' => $id	));
+	}
+
+	public function addAction(Request $request)
+	{
+		if ($request->isMethod('POST')) 
+		{
+			$request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+			return $this->redirectToRoute('ram_savon_view', array('id' => 5));
+		}
+
+		return $this->render('RamSavonBundle:Produit:add.html.twig');
+	}
+
+	public function editAction($id, Request $request)
+	{
+		if ($request->isMethod('POST')) 
+		{
+			$request->getSession()->getFlashBag()->add('notice', 'Annonce bien modifiée.');
+			return $this->redirectToRoute('ram_savon_view', array('id' => 5));
+		}
+
+		return $this->render('RamSavonBundle:Produit:edit.html.twig');
+	}
+
+	public function deleteAction($id)
+	{
+		return $this->render('RamSavonBundle:Produit:delete.html.twig');
+	}
 }
