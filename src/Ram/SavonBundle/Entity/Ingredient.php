db87b47d4216c82f3842317a75525255c0dbcbf9
rhidja <rhidja@gmail.com>
2015-11-28 01:05:55 +0100

    Création de l'entity Ingrédient

---
+++ src/Ram/SavonBundle/Entity/Ingredient.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace Ram\SavonBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Ingredient
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Ram\SavonBundle\Entity\IngredientRepository")
+ */
+class Ingredient
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=50)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="unite", type="string", length=10)
+     */
+    private $unite;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=30)
+     */
+    private $type;
+
+   /**
+    * @ORM\ManyToOne(targetEntity="Ram\SavonBundle\Entity\Categorie", cascade={"persist"})
+    */    
+    private $categorie;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Ingredient
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set unite
+     *
+     * @param string $unite
+     *
+     * @return Ingredient
+     */
+    public function setUnite($unite)
+    {
+        $this->unite = $unite;
+
+        return $this;
+    }
+
+    /**
+     * Get unite
+     *
+     * @return string
+     */
+    public function getUnite()
+    {
+        return $this->unite;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     *
+     * @return Ingredient
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set categorie
+     *
+     * @param \Ram\SavonBundle\Entity\Categorie $categorie
+     *
+     * @return Ingredient
+     */
+    public function setCategorie(\Ram\SavonBundle\Entity\Categorie $categorie = null)
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+
+    /**
+     * Get categorie
+     *
+     * @return \Ram\SavonBundle\Entity\Categorie
+     */
+    public function getCategorie()
+    {
+        return $this->categorie;
+    }
+}
