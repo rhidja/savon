285bd5665b01c549df30fa48e89687e7d1fa32b0
rhidja <rhidja@gmail.com>
2015-11-28 20:01:07 +0100

    Création du formulaire d'ajout de produit en le mappant à l'entité produit.

---
+++ src/Ram/SavonBundle/Form/ProduitType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Ram\SavonBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ProduitType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('type')
+            ->add('description')
+            ->add('dCre')
+            ->add('dMod')
+            ->add('dSup')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Ram\SavonBundle\Entity\Produit'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'ram_savonbundle_produit';
+    }
+}
