c52605cfa53ae240547c03424850b4f02720f8ee
rhidja <rhidja@gmail.com>
2016-01-17 22:33:14 +0100

    Création de l'entity Etape

---
+++ src/Ram/SavonBundle/Entity/RecetteEtape.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace Ram\SavonBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * RecetteEtape
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Ram\SavonBundle\Entity\RecetteEtapeRepository")
+ */
+class RecetteEtape
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Ram\SavonBundle\Entity\Recette")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $recette;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titre", type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="duree", type="integer")
+     */
+    private $duree;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set titre
+     *
+     * @param string $titre
+     *
+     * @return RecetteEtape
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    /**
+     * Get titre
+     *
+     * @return string
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return RecetteEtape
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set duree
+     *
+     * @param integer $duree
+     *
+     * @return RecetteEtape
+     */
+    public function setDuree($duree)
+    {
+        $this->duree = $duree;
+
+        return $this;
+    }
+
+    /**
+     * Get duree
+     *
+     * @return integer
+     */
+    public function getDuree()
+    {
+        return $this->duree;
+    }
+
+    /**
+     * Set recette
+     *
+     * @param \Ram\SavonBundle\Entity\Recette $recette
+     *
+     * @return RecetteEtape
+     */
+    public function setRecette(\Ram\SavonBundle\Entity\Recette $recette)
+    {
+        $this->recette = $recette;
+
+        return $this;
+    }
+
+    /**
+     * Get recette
+     *
+     * @return \Ram\SavonBundle\Entity\Recette
+     */
+    public function getRecette()
+    {
+        return $this->recette;
+    }
+}
